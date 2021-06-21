21b160b8b5642b4f5e710dac28e3a1e911f0b905
Jay <jayrulz99@gmail.com>
2021-06-21 21:45:25 +0100

    update xpath

--- main.py
+++ main.py
@@ -65,7 +65,16 @@ class InternetSpeedTwitterBot:
             '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[3]/div/div')
         login_button.click()
         time.sleep(2)
-        tweet = self.driver.find_element_by_name("What's happening?")
+        tweet = self.driver.find_element_by_css_selector('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.'
+                                                         'r-13qz1uu.'
+                                                         'r-417010 > main > div > div > div > div > div > div.css-'
+                                                         '1dbjc4n.r-kemksi.r-184en5c > div > div.css-1dbjc4n.r-kemksi.'
+                                                         'r-oyd9sg > div:nth-child(1) > div > div > div > div.css-'
+                                                         '1dbjc4n.r-1iusvr4.r-16y2uox.r-1777fci.r-1h8ys4a.r-1bylmt5.'
+                                                         'r-13tjlyg.r-7qyjyx.r-1ftll1t > div.css-1dbjc4n.r-184en5c > '
+                                                         'div > div > div > div > div > div > div > div > label > '
+                                                         'div.css-1dbjc4n.r-16y2uox.r-1wbh5a2 > div > div > div > div'
+                                                         ' > div.DraftEditor-editorContainer > div > div > div > div')
         tweet.click()
         message = (f"Hey Internet provider, Why is my internet speed {self.down_speed} down and {self.up_speed} "
                    f"up when I pay for {PROMISED_DOWN} and {PROMISED_UP}.")
@@ -73,7 +82,9 @@ class InternetSpeedTwitterBot:
             tweet.send_keys(f"{word} ")
             time.sleep(0.2)
         time.sleep(2)
-        tweet_button = self.driver.find_element_by_name("Tweet")
+        tweet_button = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div'
+                                                         '/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/'
+                                                         'div/div/div[2]/div[3]/div/span/span')
         time.sleep(2)
         tweet_button.click()
         print("Tweet Sent")
