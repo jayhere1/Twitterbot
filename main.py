8ffb536e074ee2a1c884be8bb0e9d7880166b36d
Jay <jayrulz99@gmail.com>
2021-06-21 21:38:03 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,86 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import time
+import os
+
+
+
+PROMISED_DOWN = 150
+PROMISED_UP = 10
+CHROME_DRIVER_PATH = "D:\Documents\Projects\chromedriver.exe"
+TWITTER_EMAIL = os.environ.get("TWITTER_EMAIL")
+TWITTER_PASSWORD = os.environ.get("TWITTER_PASSWORD")
+
+class InternetSpeedTwitterBot:
+
+    def __init__(self, driver_path):
+        self.driver = webdriver.Chrome(executable_path=driver_path)
+        self.down_speed = 0
+        self.up_speed = 0
+
+    def get_internet_speed(self):
+        self.driver.get("https://www.speedtest.net/")
+        time.sleep(2)
+        go_button = self.driver.find_element_by_css_selector("div.start-button a")
+        go_button.click()
+        time.sleep(50)
+
+        #     close AD
+        try:
+            cross_button = self.driver.find_element_by_xpath(
+                '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[8]/div/div/div[2]/a')
+            time.sleep(2)
+            cross_button.click()
+        except NoSuchElementException:
+            pass
+
+        down_speed_button = self.driver.find_element_by_xpath(
+            '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/'
+            'div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span')
+        self.down_speed = down_speed_button.text
+        up_speed_button = self.driver.find_element_by_xpath(
+            '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/'
+            'div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span')
+        self.up_speed = up_speed_button.text
+
+    def tweet_at_provider(self):
+        self.driver.get('https://twitter.com/')
+        time.sleep(5)
+        login_button = self.driver.find_element_by_xpath(
+            '//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/div[3]/a[2]')
+        login_button.click()
+        time.sleep(5)
+        username_section = self.driver.find_element_by_xpath(
+            '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[1]/label/div/div[2]/div/input')
+        username_section.click()
+        time.sleep(2)
+        username_section.send_keys(TWITTER_EMAIL)
+        time.sleep(2)
+        password_section = self.driver.find_element_by_xpath(
+            '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[2]/label/div/div[2]/div/input')
+        password_section.click()
+        password_section.send_keys(TWITTER_PASSWORD)
+        time.sleep(2)
+        login_button = self.driver.find_element_by_xpath(
+            '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[3]/div/div')
+        login_button.click()
+        time.sleep(2)
+        tweet = self.driver.find_element_by_name("What's happening?")
+        tweet.click()
+        message = (f"Hey Internet provider, Why is my internet speed {self.down_speed} down and {self.up_speed} "
+                   f"up when I pay for {PROMISED_DOWN} and {PROMISED_UP}.")
+        for word in message.split():
+            tweet.send_keys(f"{word} ")
+            time.sleep(0.2)
+        time.sleep(2)
+        tweet_button = self.driver.find_element_by_name("Tweet")
+        time.sleep(2)
+        tweet_button.click()
+        print("Tweet Sent")
+        self.driver.quit()
+
+
+bot = InternetSpeedTwitterBot(CHROME_DRIVER_PATH)
+
+bot.get_internet_speed()
+bot.tweet_at_provider()
